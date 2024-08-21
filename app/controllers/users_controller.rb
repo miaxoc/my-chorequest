17a1e82c74fff8b82e941bea05cab6807b08646d
Mia OConnell <miaxoconnell@gmail.com>
2024-08-21 16:23:24 +0900

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
