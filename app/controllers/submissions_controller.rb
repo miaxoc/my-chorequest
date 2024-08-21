c86d8ca032639b669e64e978bcf06263eaef041f
Yu Bando Foster <yu.bando.90@gmail.com>
2024-08-21 09:55:01 +0900

    Good morning

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -38,26 +38,3 @@ class SubmissionsController < ApplicationController
     params.require(:submission).permit(:status, :photo)
   end
 end
-#---------------------#
-
-# def new
-#   @bookmark = Bookmark.new
-#   @list = List.find(params[:list_id])
-# end
-
-# def create
-#   @list = List.find(params[:list_id])
-#   @bookmark = Bookmark.new(bookmark_params)
-#   @bookmark.list = @list
-#   if @bookmark.save
-#     redirect_to list_path(@list)
-#   else
-#     render 'new', status: :unprocessable_entity
-#   end
-# end
-
-# private
-
-# def bookmark_params
-#   params.require(:bookmark).permit(:comment, :movie_id)
-# end
