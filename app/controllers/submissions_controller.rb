95f1c3c2c4fdb3661716140879313891760b1774
Max McCain <itsmaximuk@gmail.com>
2024-08-22 16:56:13 +0900

    added progress bar and fixed some issues in the task index

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -27,7 +27,7 @@ class SubmissionsController < ApplicationController
     @submission.status = "completed"
     if @submission.save
       # sleep(1)
-      redirect_to tasks_path, notice: 'Submission was successfully created.'
+      redirect_to tasks_path(frequency: params[:frequency]), notice: 'Submission was successfully created.'
     else
       render 'edit', status: :unprocessable_entity, notice: 'Submission failed'
     end
