4ea2f048914c39c050483de09c1a286b4be0445c
Max McCain <itsmaximuk@gmail.com>
2024-08-27 17:24:33 +0900

    wip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,23 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import "./household-show"
+
+
+
+
+document.addEventListener("turbo:load", function() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
