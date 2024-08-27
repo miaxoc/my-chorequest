4afbc9b9191e4bc79d7e09d9dfd915e20d9e3047
Yu Bando Foster <yu.bando.90@gmail.com>
2024-08-27 15:15:54 +0900

    WIP

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -2,12 +2,12 @@ class GardensController < ApplicationController
   before_action :set_user
 
   def show
-    
+
   end
 
   private
 
   def set_user
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
   end
 end
