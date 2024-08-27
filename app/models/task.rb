1f37a43d365121dff10a1bb13d730d56b4c54bda
Mia OConnell <miaxoconnell@gmail.com>
2024-08-27 11:37:02 +0900

    categories have been altered

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :household
   belongs_to :user, optional: true
   has_many :submissions, dependent: :destroy
-  CATEGORIES = ["Vacuuming", "Kitchen", "Shopping", "5 minutes", "20 minutes", "Big Tasks"]
+  CATEGORIES = ["Vacuuming", "Kitchen", "Shopping"]
   # validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :title, presence: true
   enum frequency: ["daily", "weekly", "monthly"]
