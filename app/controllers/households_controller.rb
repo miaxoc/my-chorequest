c42449290abd912336b33a5f35ec1e4f54324182
Max McCain <itsmaximuk@gmail.com>
2024-08-28 17:29:16 +0900

    building timetable now redirects to timetable page

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -59,7 +59,7 @@ class HouseholdsController < ApplicationController
   def timetable
     @household = Household.find(params[:id])
     TimetableService.new(@household).call
-    redirect_to household_path(@household)
+    redirect_to household_calendar_path(@household)
   end
 
   def search_users
@@ -85,7 +85,7 @@ class HouseholdsController < ApplicationController
     @tasks = @household.tasks.order(created_at: :desc)
     @task = Task.new
     @members = User.all
-    
+
   end
 
   def view_tasks
