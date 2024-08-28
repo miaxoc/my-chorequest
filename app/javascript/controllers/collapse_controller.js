e596dd4e4adf370cf803a0b5576d8a8cb5106771
Max McCain <itsmaximuk@gmail.com>
2024-08-28 14:39:03 +0900

    fixed the collapse on the chores page, now it shows one or the other but not both

--- app/javascript/controllers/collapse_controller.js
+++ app/javascript/controllers/collapse_controller.js
@@ -1,11 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
-import * as bootstrap from "bootstrap"
+
 
 // Connects to data-controller="collapse"
 export default class extends Controller {
   static targets = ["form", "suggested"]
-  toggle() {
-    console.log(bootstrap)
-    
+  toggleForm() {
+    const bsCollapse = new bootstrap.Collapse(this.formTarget, { toggle: false })
+    const suggestedCollapse = new bootstrap.Collapse(this.suggestedTarget, { toggle: false })
+    bsCollapse.toggle()
+    suggestedCollapse.hide()
+  }
+
+  toggleSuggested() {
+    const bsCollapse = new bootstrap.Collapse(this.formTarget, { toggle: false })
+    const suggestedCollapse = new bootstrap.Collapse(this.suggestedTarget, { toggle: false })
+    bsCollapse.hide()
+    suggestedCollapse.toggle()
   }
 }
