94f41c006206a5132dd8e879765862657a5bc245
Max McCain <itsmaximuk@gmail.com>
2024-08-29 18:27:59 +0900

    changed some words

--- app/javascript/controllers/show_points_controller.js
+++ app/javascript/controllers/show_points_controller.js
@@ -2,14 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-points"
 export default class extends Controller {
-  static targets =["image", "form"]
+  static targets =["image", "form", "text"]
 
   showPoints() {
     console.log("connected")
     this.formTarget.classList.add("d-none");
-    this.imageTarget.classList.remove("d-none")
 
+    this.imageTarget.classList.remove("d-none");
 
+    this.textTarget.classList.add("d-none");
     // setTimeout(() => {
     //   window.location.href = "/tasks"; // Replace with the correct path
     // }, 5000)
