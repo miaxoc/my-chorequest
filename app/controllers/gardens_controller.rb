bb64a1be63e4859d12d42da917f912ae7fc3f934
Max McCain <itsmaximuk@gmail.com>
2024-08-29 16:07:53 +0900

    added pakkun

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -5,25 +5,32 @@ class GardensController < ApplicationController
     @users = current_user.household.users
     pink = ["pink1.png", 'pink2.png', 'pink1.png', 'pink3.png']
     sunflower = ["flower1.png", "flower2.png", "flower1.png", "flower3.png"]
+    pakkun = ["pakkun1.png", "pakkun2.png", "pakkun1.png", "pakkun2.png"]
 
-    @array = [pink, sunflower].sample
-    @array2 = [pink, sunflower].sample
+    @array = [pink, sunflower, pakkun].sample
+    @array2 = [pink, sunflower, pakkun].sample
 
     if @array == pink
       @inventory_image = "pink1.png"
     elsif @array == sunflower
       @inventory_image = "flower1.png"
+    elsif @array == pakkun
+      @inventory_image = "pakkun1.png"
     else
       @inventory_image = "bud1.png"
     end
 
     if @array2 == pink
       @inventory_image2 = "pink1.png"
-    elsif @array == sunflower
+    elsif @array2 == sunflower
       @inventory_image2 = "flower1.png"
+    elsif @array2 == pakkun
+      @inventory_image2 = "pakkun1.png"
     else
       @inventory_image2 = "bud1.png"
     end
+
+
   end
 
   def water
